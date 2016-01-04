61fe0fc8bdc08b96cec6839bb4c31104ac2a4c6c
Leviathan Girl <job@catscarlet.com>
2016-01-04 14:41:41 +0800

    rewriting

--- web/Public/shell/needforping.sh
+++ web/Public/shell/needforping.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-needforping_DIR=/var/www/html/8091/needforpingthinksmarty/Public/
-. /$needforping_DIR/shell/needforping.conf
+needforping_DIR=/var/www/need4ping/
 
-TMP_DIR=/$needforping_DIR/shell/pingresult
-#SERVER_LIST=/$needforping_DIR/shell/server_list.txt
+needforping_Shell_DIR=$needforping_DIR/web/Public/shell/
+. $needforping_Shell_DIR/needforping.conf
+TMP_DIR=$needforping_Shell_DIR/pingresult
 
     i=0
-    MYSQL_COMMAND="SELECT server_name FROM $DB_PREFIX$DB_NAME.pinglist ;"
-    mysql -u$DB_USER -p$DB_PWD -e "$MYSQL_COMMAND" | while read server_name; do
-
+MYSQL_COMMAND="SELECT server_name FROM $DB_PREFIX$DB_NAME.pinglist ;"
+mysql -u$DB_USER -p$DB_PWD -e "$MYSQL_COMMAND" | while read server_name;
+  do
     if [ $i -ne 0 ] ;then
-      $needforping_DIR/shell/goping.sh $server_name &
-      #echo "$server_name"
+    $needforping_Shell_DIR/goping.sh $server_name &
     fi
     i=`expr $i + 1`
-      done
+  done
